c1f17910b230094082fc8f6b6cf9046240e98890
Bram Nijssen <bram@brik.digital>
2025-07-11 13:55:48 +0200

    Initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace brikdigital\craftlerendoejenu;
+
+class Plugin extends \craft\base\Plugin
+{
+    public bool $hasCpSettings = true;
+
+    public function init(): void
+    {
+        parent::init();
+    }
+}
