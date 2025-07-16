02f8cde542ef54c351fa2b4cb098409178853d6a
Bram Nijssen <bram@brik.digital>
2025-07-16 11:01:15 +0200

    Delete deleted Teachers

--- src/jobs/SyncTeachers.php
+++ src/jobs/SyncTeachers.php
@@ -18,10 +18,11 @@ class SyncTeachers extends BaseJob
     public function execute($queue): void
     {
         $count = 0;
+        $processedIds = [];
 
         Plugin::getInstance()->api->getAll('teachers', [
             'sort' => 'teacher_id,asc'
-        ], function ($response) use ($count, $queue) {
+        ], function ($response) use (&$count, &$processedIds, $queue) {
             foreach ($response['content'] ?? [] as $teacher) {
                 $queue->setProgress(
                     round(100 * (++$count / $response['page']['total_elements'])),
@@ -31,8 +32,15 @@ class SyncTeachers extends BaseJob
                     ])
                 );
 
-                $element = new Teacher();
-                $element->foreignId = $teacher['user_id'];
+                // Try to find existing element
+                $element = Teacher::find()
+                    ->foreignId($teacher['user_id'])
+                    ->one();
+                if (!$element) {
+                    $element = new Teacher();
+                    $element->foreignId = $teacher['user_id'];
+                }
+
                 $element->firstName = $teacher['first_name'];
                 $element->infix = $teacher['infix'];
                 $element->lastName = $teacher['last_name'];
@@ -40,7 +48,22 @@ class SyncTeachers extends BaseJob
                 $element->imageUrl = $teacher['image_url'];
 
                 Craft::$app->getElements()->saveElement($element);
+
+                $processedIds[] = $element->id;
             }
         });
+
+        $this->deleteTeachers($processedIds);
+    }
+
+    private function deleteTeachers(array $processedIds): void
+    {
+        $teachersToDelete = Teacher::find()
+            ->id(array_merge(['not'], $processedIds))
+            ->all();
+
+        foreach ($teachersToDelete as $teacher) {
+            Craft::$app->getElements()->deleteElement($teacher, true);
+        }
     }
 }
